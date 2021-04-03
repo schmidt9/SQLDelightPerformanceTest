a76acf4dca87b534ffa02dc3a14e3f59a71f757a
Alexander Kormanovsky <kormanowskij@yandex.ru>
2021-04-03 14:41:51 +0300

    refactor

--- iosApp/iosApp/ViewController.swift
+++ iosApp/iosApp/ViewController.swift
@@ -13,17 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let dbPath = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!.appendingPathComponent("databases").appendingPathComponent("test.db")
+        
         print("\n=== Kotlin ===\n")
         
-        let dbPath = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first!.appendingPathComponent("databases")
-        print("SQLDelight databases path", dbPath.path)
-        
+        print("SQLDelight test database path", dbPath.path)
+
         print("start createProjects()")
         var startDate = Date()
         DatabaseTest().createProjects()
         var endDate = Date()
         print("createProjects() elapsed time", endDate.timeIntervalSince(startDate))
-        
+
         print("start fetchProjects()")
         startDate = Date()
         let kotlinProjects = DatabaseTest().fetchProjects()
