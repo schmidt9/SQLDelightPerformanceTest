326fc94c5960a856af95e7e67a983c6d3ade5473
Alexander Kormanovsky <kormanowskij@yandex.ru>
2021-04-28 11:03:21 +0300

    update gradle & sqldelight

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
-        classpath("com.android.tools.build:gradle:4.2.0-beta06")
-        classpath("com.squareup.sqldelight:gradle-plugin:1.4.4")
+        classpath("com.android.tools.build:gradle:4.2.0-rc01")
+        classpath("com.squareup.sqldelight:gradle-plugin:1.5.0")
     }
 }
 
