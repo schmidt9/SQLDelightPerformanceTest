fee23cb34686b208276d89983b6c2943d7163124
Alexander Kormanovsky <kormanowskij@yandex.ru>
2024-04-27 17:04:44 +0300

    impl states;

--- shared/src/commonMain/kotlin/com/example/sqldelightperformancetest/shared/HomeScreen.kt
+++ shared/src/commonMain/kotlin/com/example/sqldelightperformancetest/shared/HomeScreen.kt
@@ -1,10 +1,19 @@
 package com.example.sqldelightperformancetest.shared
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.model.rememberScreenModel
@@ -15,11 +24,36 @@ class HomeScreen : Screen {
     @Composable
     override fun Content() {
         val screenModel = rememberScreenModel { HomeScreenModel() } // TODO: use
+        val screenModelState by screenModel.state.collectAsState()
+        var isRunningTest by remember { mutableStateOf(false) }
 
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             AnimatedVisibility(true) {
+                when (val state = screenModelState) {
+                    is HomeScreenModel.State.RunningTest -> {
+                        isRunningTest = state.isRunning
+                    }
+                    is HomeScreenModel.State.Result -> {
+                        // TODO: impl
+                    }
+                }
                 Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text("TEST")
+                    Button(onClick = {
+                        screenModel.runRecordsTest()
+                    }, enabled = !isRunningTest) {
+                        Text("Run records test")
+                    }
+
+                    if (isRunningTest) {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center) {
+                            CircularProgressIndicator()
+                            Text("Running test")
+                        }
+                    }
+
                 }
             }
         }
--- shared/src/commonMain/kotlin/com/example/sqldelightperformancetest/shared/HomeScreenModel.kt
+++ shared/src/commonMain/kotlin/com/example/sqldelightperformancetest/shared/HomeScreenModel.kt
@@ -1,12 +1,28 @@
 package com.example.sqldelightperformancetest.shared
 
 import cafe.adriel.voyager.core.model.StateScreenModel
+import cafe.adriel.voyager.core.model.screenModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class HomeScreenModel : StateScreenModel<HomeScreenModel.State>(State.Loading) {
+class HomeScreenModel : StateScreenModel<HomeScreenModel.State>(State.RunningTest(false)) {
 
     sealed class State {
-        data object Loading : State()
-        data class Result(val projects: List<Any>) : State()
+        data class RunningTest(val isRunning: Boolean) : State()
+        data class Result(val records: List<Any>) : State()
+    }
+
+    fun runRecordsTest() {
+        screenModelScope.launch(context = Dispatchers.IO) {
+            mutableState.value = State.RunningTest(isRunning = true)
+            delay(2000)
+            val records = listOf<Any>() // TODO: impl
+            mutableState.value = State.RunningTest(isRunning = false)
+            mutableState.value = State.Result(records)
+        }
     }
 
 }
