8aea1648b7b3c3a82407e47fb7c2366a5b33884b
Alexander Kormanovsky <kormanowskij@yandex.ru>
2024-04-28 00:18:15 +0300

    cleanup; add TimeUtils;

--- shared/src/commonMain/kotlin/com/example/sqldelightperformancetest/shared/HomeScreenModel.kt
+++ shared/src/commonMain/kotlin/com/example/sqldelightperformancetest/shared/HomeScreenModel.kt
@@ -2,6 +2,7 @@ package com.example.sqldelightperformancetest.shared
 
 import cafe.adriel.voyager.core.model.StateScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
+import com.example.db.DatabaseTest
 import io.github.aakira.napier.Napier
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -32,5 +33,39 @@ class HomeScreenModel : StateScreenModel<HomeScreenModel.State>(State.RunningTes
             mutableState.value = State.Result(records)
         }
     }
+
+//    private fun fetchTestResults(): String {
+//        val databaseTest = DatabaseTest(this)
+//        val builder = StringBuilder()
+//
+//        val nativeCreateProjectsTime = measureTimeMillis {
+//            databaseTest.createProjects()
+//        }
+//
+//        val nativeFetchProjectsTime = measureTimeMillis {
+//            databaseTest.fetchProjects()
+//        }
+//
+//        val cppCreateProjectsTime = measureTimeMillis {
+//            CppTestDatabase.createProjects()
+//        }
+//
+//        val cppFetchProjectsTime = measureTimeMillis {
+//            val projects = CppTestDatabase.fetchProjects()
+//            Log.d("Tests", "Projects count ${projects.count()}")
+//        }
+//
+//        builder.append(if (BuildConfig.DEBUG) "\nRunning Debug" else "\nRunning Release").append("\n\n")
+//
+//        builder.append("nativeCreateProjectsTime: ${nativeCreateProjectsTime / 1000.0}\n")
+//        builder.append("nativeFetchProjectsTime: ${nativeFetchProjectsTime / 1000.0}\n\n")
+//
+//        builder.append("cppCreateProjectsTime: ${cppCreateProjectsTime / 1000.0}\n")
+//        builder.append("cppFetchProjectsTime: ${cppFetchProjectsTime / 1000.0}")
+//
+//        Log.d("Tests", "$builder")
+//
+//        return builder.toString()
+//    }
 
 }
