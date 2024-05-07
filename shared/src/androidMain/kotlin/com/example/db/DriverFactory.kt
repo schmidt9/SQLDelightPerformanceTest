1f42dd7694cd9cfb8d9b88860f166605e90066c7
Alexander Kormanovsky <kormanowskij@yandex.ru>
2024-05-07 16:58:37 +0300

    add comment;

--- shared/src/androidMain/kotlin/com/example/db/DriverFactory.kt
+++ shared/src/androidMain/kotlin/com/example/db/DriverFactory.kt
@@ -8,6 +8,12 @@ import com.example.sqldelightperformancetest.shared.AppContext
 actual class DriverFactory actual constructor() {
     actual fun createDriver(): SqlDriver {
         val appContext = AppContext.get()
-        return AndroidSqliteDriver(TestDatabase.Schema, appContext, "test.db")
+        return AndroidSqliteDriver(
+            TestDatabase.Schema,
+            appContext,
+            "test.db",
+            // fix CursorWindow (working since API 28)
+            // https://github.com/cashapp/sqldelight/pull/4804
+            windowSizeBytes = 10_000_000)
     }
 }
