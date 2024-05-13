9549a57f5cfb4f68bddc1a76eb7073f899c3fb7f
Alexander Kormanovsky <kormanowskij@yandex.ru>
2024-05-13 12:01:48 +0300

    roll back image part queries;

--- shared/src/commonMain/kotlin/com/example/db/DatabaseTest.kt
+++ shared/src/commonMain/kotlin/com/example/db/DatabaseTest.kt
@@ -37,60 +37,24 @@ class DatabaseTest() {
         }
     }
 
-    @OptIn(ExperimentalStdlibApi::class)
     fun fetchProjects(): List<Project> {
         val queries = database.dataQueries
         val identifiersQuery = queries.fetchProjectIdentifiers()
         val identifiersList = identifiersQuery.executeAsList()
 
+        // we fetch projects one by one to avoid error:
+        // Window is full: requested allocation 3427723 bytes, free space 2096650 bytes, window size 2097152 bytes
+        // https://stackoverflow.com/questions/20094421/cursor-window-window-is-full
+        // try https://github.com/andpor/react-native-sqlite-storage/issues/364#issuecomment-526423153
         val projects = identifiersList.map {
-            val projectWithoutImageQuery = queries.fetchProjectWithoutImage(it)
-            val projectImageLengthQuery = queries.fetchProjectImageLength(it)
-            // FIXME: fix crash NullPointerException due to too big image size (~3.4mb)
-            // Window is full: requested allocation 3427723 bytes, free space 2096650 bytes, window size 2097152 bytes
-            // https://stackoverflow.com/questions/20094421/cursor-window-window-is-full
-            // try https://github.com/andpor/react-native-sqlite-storage/issues/364#issuecomment-526423153
-            val project = projectWithoutImageQuery.executeAsOne()
-            val imageLength = projectImageLengthQuery.executeAsOne()
-            val length = imageLength.length
+            val projectQuery = queries.fetchProject(it)
+            val project = projectQuery.executeAsOne()
 
-            val image = if (length != null && length > 0) {
-                val windowSize = (1024 * 1024 * 1.5).toInt()
-                val windowParts = length.toInt() / windowSize
-                var from = 0
-                var to = windowSize
-                var result: ByteArray? = null
-
-                for (index in 0 until windowParts) {
-                    val _from = from.toString()
-                    val _to = to.toString()
-                    val imagePartQuery = queries.fetchPartProjectImage(_from, _to, it)
-                    val image = imagePartQuery.executeAsOne()
-
-                    result = result?.plus(image) ?: image
-
-                    from = to
-                    to = if (index + 1 == windowParts) length.toInt() else to + windowSize
-                }
-
-                result
-            } else {
-                null
-            }
-
-            val size = image?.size
-
-            return@map Project(
-                project._id,
-                project.name,
-                project.created,
-                project.update_time,
-                project.is_active,
-                image
-            )
+            return@map project
         }
 
         return projects
     }
+
 
 }
